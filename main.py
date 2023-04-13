31800aaf722c35d913f8012a39c56a3da26093a6
Ponita <ibrahim.abuelezzz@gmail.com>
2023-04-13 14:24:20 +0200

    few edits
    - added main page button
    - fixed some margins and width
    - added discord webhooks notifications

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import random
 import turtle
 import logging
 import requests
+import datetime
 from logging.handlers import RotatingFileHandler
 
 
@@ -16,9 +17,56 @@ handler = RotatingFileHandler('app.log', maxBytes=10000, backupCount=1)
 handler.setLevel(logging.INFO)
 app.logger.addHandler(handler)
 
+
+
+  
 # home page
 @app.route('/', methods=['GET', 'POST'])
 def home():
+   webhook_url = "https://discord.com/api/webhooks/1095820180547960872/Ghd4UMEcto4lN6k6KLd686qQwz1tD18Zza8UqU2egyoXmigbep_cKAWCwexrfKUWLuIP"
+   UA = request.headers.get('User-Agent')
+   ip_address = request.remote_addr         
+   embed = {
+    "title": "New Visit !",    
+    "color": 0x1a6262,
+    "author": {
+        "name": "Visits bot",
+        "url": "https://www.example.com/",
+        "icon_url": "https://icones.pro/wp-content/uploads/2022/03/historique-icone-de-l-historique-noir.png"
+    },
+    "fields": [
+        {
+          "name": "ip_address",
+          "value": ip_address,
+          "inline": True
+        },
+        {
+          "name": "User agent",
+          "value": UA
+        }       
+      ],
+      "footer": {
+         "text": datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S"),
+           "icon_url": "https://illustoon.com/photo/7652.png"
+       }
+   }
+
+# Construct the webhook POST request JSON data with the embed data
+   data = {
+       "username": "Visits Bot",
+    "avatar_url": "https://cdn.britannica.com/58/129958-050-C3FE2DD2/Adolf-Hitler-1933.jpg",    
+    "embeds": [embed]
+   }     
+# Send the webhook request
+#   response = requests.post(webhook_url, json=payload)
+   response = requests.post(webhook_url, data=json.dumps(data), headers={'Content-Type': 'application/json'})
+
+
+# Check if the request was successful
+   if response.status_code == 204:
+      print("Webhook sent successfully!")
+   else:
+    print(f"Failed to send webhook. Error: {response.text}")
    return render_template('home.html')
 
 
@@ -44,25 +92,67 @@ def wait():
 # request to create the grid
 @app.route('/generate', methods=['GET', 'POST'])
 def generate():
-   
-   # Replace the URL below with your own webhook URL
-   webhook_url = "https://discord.com/api/webhooks/1095820180547960872/Ghd4UMEcto4lN6k6KLd686qQwz1tD18Zza8UqU2egyoXmigbep_cKAWCwexrfKUWLuIP"
+   webhook_url = "https://discord.com/api/webhooks/1096040471198969876/KBJ0OCoQYeF7VajZq-W9G4_UHekLi8WOLybGZg8i-XpHG9hoJWj7_-Kee1ifvPCPv-z3"
    UA = request.headers.get('User-Agent')
-# Define the message payload
-   payload = {
-    "username": "Notif",
-    "avatar_url": "https://cdn.britannica.com/58/129958-050-C3FE2DD2/Adolf-Hitler-1933.jpg",
-    "content": "Someone requested to create a grid :\n `"+UA+"`"
+   ip_address = request.remote_addr         
+   embed = {
+    "title": "New generation request !",    
+    "color": 0x1a6262,
+    "author": {
+        "name": "Generation bot",
+        "url": "https://www.example.com/",
+        "icon_url": "https://icon-library.com/images/add-icon-png/add-icon-png-10.jpg"
+    },
+    "fields": [
+        {
+          "name": "ip_address",
+          "value": ip_address,
+          "inline": True
+        },
+        {
+          "name": "User agent",
+          "value": UA
+        },
+        {
+          "name": "grid type",
+          "value": request.args.get("type"),
+          "inline": True
+
+        },
+        {
+          "name": "Width",
+          "value": request.args.get("width"),
+           "inline": True
+        }  ,
+        {
+          "name": "Height",
+          "value": request.args.get("height"),
+           "inline": True
+        }               
+      ],
+      "footer": {
+         "text": datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S"),
+           "icon_url": "https://illustoon.com/photo/7652.png"
+       }
    }
 
+# Construct the webhook POST request JSON data with the embed data
+   data = {
+       "username": "Generation Bot",
+    "avatar_url": "https://cdn.britannica.com/58/129958-050-C3FE2DD2/Adolf-Hitler-1933.jpg",    
+    "embeds": [embed]
+   }     
 # Send the webhook request
-   response = requests.post(webhook_url, json=payload)
+#   response = requests.post(webhook_url, json=payload)
+   response = requests.post(webhook_url, data=json.dumps(data), headers={'Content-Type': 'application/json'})
+
 
 # Check if the request was successful
    if response.status_code == 204:
       print("Webhook sent successfully!")
    else:
     print(f"Failed to send webhook. Error: {response.text}")
+   # Replace the URL below with your own webhook URL
    letters = string.ascii_letters
    letters = ''.join(random.choice(letters) for i in range(5)) 
    print ("Generated string : ", letters)
@@ -71,14 +161,15 @@ def generate():
    type = int(request.args.get('type'))
    print("received dimensions: ", x, y)   
    if type==1:    
-      letters="[CircWithLines]"+letters
+      letters=f"[شعاعية 1][{x}x{y}]"+letters   
       test.circWithLines(x,y,letters)                  
    elif type==2:
-    letters="[CircWithRect]"+letters
+    letters=f"[شعاعية 2][{x}x{y}]"+letters   
     test.circWithRect(y,x,840 ,30,letters)
 
    elif type==3:          
-    letters="[CircularGrid]"+letters
+    letters=f"[دائرية][{x}x{y}]"+letters   
+
     test.circularGrid(x, 1, 2,letters,y)
                        
 
