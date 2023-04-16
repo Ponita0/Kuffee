d020f61bf4393d4549010fe08f89f9a5d803ec3a
Ibrahim Abdelmonem <34416796+Ponita0@users.noreply.github.com>
2023-04-16 04:23:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -204,5 +204,5 @@ def download():
 if __name__ == '__main__':
    #  app.run(host="0.0.0.0", port=80, debug=True)
     from waitress import serve
-    serve(app, host="0.0.0.0", port=80)
+    serve(app, host="0.0.0.0", port=80,static_folder='static')
 
